82ad4ccd64a13d765b4bde3dba92742a16c0f1c2
Mark Strefford <mark@m20ventures.co.uk>
2018-07-22 22:11:49 +0100

    Adding comments / tidying code

--- 1. Asking natural language queries/Source/crawler/crawl_wikipedia.py
+++ 1. Asking natural language queries/Source/crawler/crawl_wikipedia.py
@@ -1,5 +1,6 @@
 """
-
+Download content for a specified category on Wikipedia into a local database
+This text corpus will be used for topic modelling
 """
 import sqlite3
 import wptools
@@ -8,23 +9,30 @@ from bs4 import BeautifulSoup
 
 class crawl_wikipedia:
     def __init__(self, db_file):
+        """
+        Intialise the crawl_wikipedia class, set up a
+        lightweight database for storing content for later use
+        :param db_file:
+        """
         self.categories = []
-        # self.pageids=[]
-        self.count = 0
         # Create db
         self.conn = sqlite3.connect(db_file)
         c = self.conn.cursor()
         c.execute('''CREATE TABLE IF NOT EXISTS content
-            (pageid text, url text, content text)''')
+            (category text, pageid text, url text, content text)''')
         self.conn.commit()
         self.cursor = self.conn.cursor()
 
-    def _save_page_content(self, pageid, url, content):
-        self.cursor.execute("INSERT INTO content VALUES (?,?,?)",
-                            (pageid, url, content))
+    def _save_page_content(self, category, pageid, url, content):
+        self.cursor.execute("INSERT INTO content VALUES (?, ?,?,?)",
+                            (category, pageid, url, content))
         self.conn.commit()
 
-    def get_page_content(self, columns='*'):
+    def get_page_urls(self):
+        """
+        Retrieve a list of urls from the database
+        :return:
+        """
         # TODO - Add in ability to select single columns
         output = []
         for row in self.cursor.execute("SELECT url FROM content"):
@@ -33,15 +41,23 @@ class crawl_wikipedia:
         return output
 
     def get_categories_and_members(self, category, depth):
+        """
+        Start with the defined category and download Wikipedia content
+        up to the specific depth of categories
+        :param category:
+        :param depth:
+        :return:
+        """
         print('Checking for subcategories of {} at depth {}'.format(category, depth))
         if depth:
+            # Get details of this category
+            # Members are pages related to this category
             cat = wptools.category(category)
             cat_members = cat.get_members()
             # First let's save any members (pages) for this category
             if 'members' in cat_members.data.keys():
                 for cat_member in cat_members.data['members']:
-                    # print('Appending {} to pageids'.format(cat_member['pageid']))
-                    # self.pageids.append(cat_member['pageid'])
+                    # Get the page content
                     page = wptools.page(pageid=cat_member['pageid']).get_parse()
                     # Remove <ref> and other HTML syntax
                     text = BeautifulSoup(page.data['wikitext'], 'html.parser').get_text()
@@ -51,12 +67,11 @@ class crawl_wikipedia:
                     url = page.get_query().data['url']
                     # Now store
                     print('Saving pageid {} / url {}'.format(cat_member['pageid'], url))
-                    self._save_page_content(cat_member['pageid'], url, clean_content)
+                    self._save_page_content(category, cat_member['pageid'], url, clean_content)
             # Now iterate through any subcategories
             if 'subcategories' in cat_members.data.keys():
                 subcats = cat_members.data['subcategories']
                 for subcat in subcats:
                     self.categories.append(subcat)
-                    self.count += 1
-                    # print('Appending {} / count = {}'.format(subcat['title'], self.count))
+                    # Recursively call this function until we've explored Wikipedia up to the specified depth
                     self.get_categories_and_members(subcat['title'], depth - 1)
--- 1. Asking natural language queries/Source/nlp_demo.py
+++ 1. Asking natural language queries/Source/nlp_demo.py
@@ -1,15 +1,27 @@
 """
+Learning Python AI by Example: Asking Natural Language Queries
+
+Using Wikipedia Artificial Intelligence content to create a model that we can query using natural language
+to get rich information and responses from.
 
 
 """
 
-from crawler.crawler_wikipedia import crawl_wikipedia
+# Imports
+from crawler.crawl_wikipedia import crawl_wikipedia
 
+# Configuration:
+# Start with the top level Artificial Intelligence category on Wikipedia
 category = 'Category:Artificial_intelligence'
+
+# Only go down 1 level in subcategories
 depth = 1
+
+# Set up a simple database so we can use this data later
 crawler = crawl_wikipedia('content.db')
 
+#Now use the Wikipedia API to populate our database
 crawler.get_categories_and_members(category, depth)
-crawler.get_page_content()
+
 
 
