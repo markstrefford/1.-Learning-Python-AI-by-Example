9afc43697e5206942f1138be46aa9a3a20a304cb
Mark Strefford <mark@m20ventures.co.uk>
2018-10-02 13:42:09 +0100

    Multi-face recognition
    Added ability to detect single face for training or multiple faces for inferrence
    Tidied up code

--- 2. Recognising Faces/Source/opencv_face_recognition/opencv_face_recognition.py
+++ 2. Recognising Faces/Source/opencv_face_recognition/opencv_face_recognition.py
@@ -25,7 +25,6 @@ class face_recognition:
                  embedding_model='./opencv_face_recognition/openface/nn4.small2.v1.t7',
                  labels_file=False, recogniser_file=False):
         """
-
         :param prototxt_path: string Path to deploy.protext
         :param model_path: string Path to trained caffe model
         :param embedding_model: string Path to OpenFace model
@@ -45,66 +44,115 @@ class face_recognition:
         else:
             self.recogniser = SVC(C=1.0, kernel="linear", probability=True)
 
-    def get_face_embeddings_from_image(self, image):
+    def _detect_faces(self, image, all=False):
+        """
+        Use OpenCV face detector to return face ROI (detections) from an image.
+        :param image:  np.array - image
+        :return: num_detections, detections:  int, np.array
+        """
+        resized_image = cv2.resize(image, (300, 300))
+        image_blob = cv2.dnn.blobFromImage(resized_image, 1.0, (300, 300),
+                                           mean_subtract_values, swapRB=False, crop=False)
+        self.detector.setInput(image_blob)
+        detections = self.detector.forward()
+        num_detections = detections.shape[2]
+        return num_detections, detections
+
+    def _get_face_vec(self, face):
+        """
+        Get the vector for this face
+        :param face: image containing the face
+        :return: vector
+        """
+        face_blob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+                                          (96, 96), (0, 0, 0), swapRB=False, crop=False)    # swapRB=True
+        self.embedder.setInput(face_blob)
+        return self.embedder.forward()
+
+    def _get_face_bbox(self, detection, image_dimensions):
+        """
+        Get the coordinates for the face bounding box from this detection
+        :param detection: The OpenCV detection for this face
+        :param image_dimensions:  We need this to scale the bbox accordingly
+        :return:
+        """
+        (h, w) = image_dimensions   #image.shape[:2]
+        box = detection[3:7] * np.array([w, h, w, h])
+        start_x, start_y, end_x, end_y = box.astype("int")
+        return start_x, start_y, end_x, end_y
+
+    def get_face_embeddings_from_image(self, image, all=False):
         """
         Return the bbox and the embeddings vector for the face in the image
         (Note we return this for the face that has the highest confidence from the detector)
         :param image: np.array
+        :param all: Boolean  - return face with highest confidece (False) or all detected faces (true)
         :return: tuple, array
         """
+        results = []
         vec = []
         start_x, start_y, end_x, end_y = 0, 0, 0, 0
 
-        resized_image = cv2.resize(image, (300, 300))
-        image_blob = cv2.dnn.blobFromImage(resized_image, 1.0, (300, 300),
-            mean_subtract_values, swapRB=False, crop=False)
-        (h, w) = image.shape[:2]
+        # Detect faces in the image
+        num_detections, detections = self._detect_faces(image)
+        # As this is for training our classifier, take the face with the highest confidence
+        if num_detections:
+            if not all:
+                d = np.argmax(detections[0, 0, :, 2])
+                detections = detections[:, :, [d], :]
+                num_detections = 1
+            # Loop over detected faces
+            for i in range(0, num_detections):
+                conf = detections[0, 0, i, 2]
 
-        # apply OpenCV's deep learning-based face detector to localize faces in the input image
-        self.detector.setInput(image_blob)
-        predictions = self.detector.forward()
-        # ensure at least one face was found
-        if len(predictions) > 0:
-            # we're making the assumption that each image has only ONE
-            # face, so find the bounding box with the largest probability
-            i = np.argmax(predictions[0, 0, :, 2])
-            conf = predictions[0, 0, i, 2]
-
-            # ensure that the detection with the largest probability also
-            # means our minimum probability test (thus helping filter out
-            # weak detections)
-            if conf > confidence:
-                # compute the (x, y)-coordinates of the bounding box for
-                # the face
-                box = predictions[0, 0, i, 3:7] * np.array([w, h, w, h])
-                start_x, start_y, end_x, end_y = box.astype("int")
-
-                # extract the face ROI and grab the ROI dimensions
-                face = image[start_y:end_y, start_x:end_x]
-
-                # ensure the face width and height are sufficiently large
-                if face.shape[0] >= 20 and face.shape[1] > 20:
-                    # construct a blob for the face ROI, then pass the blob
-                    # through our face embedding model to obtain the 128-d
-                    # quantification of the face
-                    face_blob = cv2.dnn.blobFromImage(face, 1.0 / 255,
-                                                     (96, 96), (0, 0, 0), swapRB=True, crop=False)
-                    self.embedder.setInput(face_blob)
-                    vec = self.embedder.forward()
-        return (start_x, start_y, end_x, end_y), vec
+                # Check OpenCV has a high enough confidence in this being a face
+                if conf > confidence:
+                    start_x, start_y, end_x, end_y = self._get_face_bbox(detections[0, 0, i], image.shape[:2])
+                    # Extract this face from the image and if it is sufficiently large, get the vector for it
+                    face = image[start_y:end_y, start_x:end_x]
+                    if face.shape[0] >= 20 and face.shape[1] > 20:
+                        vec = self._get_face_vec(face)
+                        results.append(((start_x, start_y, end_x, end_y), vec))
+        return results
 
     def _build_embeddings(self, X, y):
         for i, (image_path, name) in enumerate(zip(X, y)):
             image = load_image_as_array(image_path)
             print('Processing image {}'.format(image_path))
-            _, face_embedding_vector = self.get_face_embeddings_from_image(image)
+            _, face_embedding_vector = self.get_face_embeddings_from_image(image, all=False)[0]
             if face_embedding_vector != []:
                 self.embeddings['names'].append(name)
                 self.embeddings['embeddings'].append(face_embedding_vector[0])
 
+    def recognise(self, image):
+        """
+        Predict all faces in an image
+        :param image: np.array containing image
+        :return: np.array containing image, bbox and labels
+        """
+        # box, vec = self.get_face_embeddings_from_image_multi(image)
+        results = self.get_face_embeddings_from_image(image, all=True)
+        if len(results):
+            for box, vec in results:
+                predictions = self.recogniser.predict_proba(vec)[0]
+                if len(predictions) > 0:
+                    j = np.argmax(predictions)
+                    proba = predictions[j]
+                    name = self.le.classes_[j]
+
+                    start_x, start_y, end_x, end_y = box
+                    text = "{}: {:.2f}%".format(name, proba * 100)
+                    y = start_y - 10 if start_y - 10 > 10 else start_y + 10
+                    cv2.rectangle(image, (start_x, start_y), (end_x, end_y),
+                                  (0, 0, 255), 2)
+                    cv2.putText(image, text, (start_x, y),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        return image
+
+
     def train(self, X, y):
         """
-        Train the model based on the data provided
+        Train the SVM model based on the data provided
         :param X: list of filenames
         :param y: labels
         :return:
@@ -114,32 +162,3 @@ class face_recognition:
         self.labels = self.le.fit_transform(self.embeddings['names'])
         self.recogniser.fit(self.embeddings['embeddings'], self.labels)
         print('Training complete')
-
-    def recognise(self, image):
-        """
-        Predict all faces in an image
-        :param image: np.array containing image
-        :return: np.array containing image, bbox and labels
-        """
-        box, vec = self.get_face_embeddings_from_image(image)
-        if len(vec):
-            predictions = self.recogniser.predict_proba(vec)[0]
-            if len(predictions) > 0:
-                j = np.argmax(predictions)
-                proba = predictions[j]
-                name = self.le.classes_[j]
-
-                start_x, start_y, end_x, end_y = box
-                text = "{}: {:.2f}%".format(name, proba * 100)
-                y = start_y - 10 if start_y - 10 > 10 else start_y + 10
-                cv2.rectangle(image, (start_x, start_y), (end_x, end_y),
-                              (0, 0, 255), 2)
-                cv2.putText(image, text, (start_x, y),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-        return image
-
-
-
-
-
-
