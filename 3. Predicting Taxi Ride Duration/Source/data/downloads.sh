c623ce39cd1ac717f150fca2e87b1dc7cabf40d3
Mark Strefford <markstrefford@gmail.com>
2018-11-15 15:31:24 +0000

    mapping data

--- 3. Predicting Taxi Ride Duration/Source/data/downloads.sh
+++ 3. Predicting Taxi Ride Duration/Source/data/downloads.sh
@@ -35,12 +35,31 @@ wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-04.csv -P ./
 wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-05.csv -P ./taxi_data
 wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-06.csv -P ./taxi_data
 
+#Consolidate yellow taxi data and extract relevant info
+echo "VendorID,tpep_pickup_datetime,tpep_dropoff_datetime,passenger_count,trip_distance,RatecodeID,PULocationID,DOLocationID,fare_amount,tolls_amount,total_amount" > ./taxi_data/consolidated_yellow_tripdata.csv
+ls ./taxi_data/*yellow_tripdata* | grep -v consolidated | while read f;
+do
+echo ${f}
+grep '^[0-9]' ${f} | awk -F',' -v OFS=',' '{print $1,$2,$3,$8,$9,$5,$6,$7,$10,$14,$17}' >> ./taxi_data/consolidated_yellow_tripdata.csv
+done
+
+#Consolidate green taxi data and extract relevant info
+echo "VendorID,tpep_pickup_datetime,tpep_dropoff_datetime,passenger_count,trip_distance,RatecodeID,PULocationID,DOLocationID,fare_amount,tolls_amount,total_amount" > ./taxi_data/consolidated_green_tripdata.csv
+ls ./taxi_data/*green_tripdata* | grep -v consolidated | while read f;
+do
+echo ${f}
+grep '^[0-9]' ${f} | awk -F',' -v OFS=',' '{print $1,$2,$3,$4,$5,$6,$8,$9,$11,$15,$17}' >> ./taxi_data/consolidated_green_tripdata.csv
+done
+
 #Weather data
 #Open NYC weather data from https://www.ncdc.noaa.gov/
 #TODO - Make a curl command to download the data
 
 #Lat/long by address
-#Open address data from ...
+#Open address data from http://results.openaddresses.io
+#See also https://github.com/openaddresses/openaddresses/blob/adaa71fc0affa5c3f577b85825d341871cf3d3bd/sources/us/ny/city_of_new_york.json
+#and https://data.cityofnewyork.us/City-Government/NYC-Address-Points/g6pj-hd8k
 #TODO - Make a curl command to download the data (if needed??)
+
 
 
