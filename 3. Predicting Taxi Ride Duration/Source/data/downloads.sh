e5d4680265a1c76a6a70826e123e018cfd7f89d8
Mark Strefford <markstrefford@gmail.com>
2018-11-17 19:22:24 +0000

    Moving large csv files to lfs

--- 3. Predicting Taxi Ride Duration/Source/data/downloads.sh
+++ 3. Predicting Taxi Ride Duration/Source/data/downloads.sh
@@ -3,9 +3,9 @@
 #Open taxi data from http://www.nyc.gov/html/tlc/html/about/trip_record_data.shtml
 
 #Taxi zone data
-wget https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv -P ./geo_data
-wget https://s3.amazonaws.com/nyc-tlc/misc/taxi_zones.zip
-unzip ./taxi_zones.zip
+#wget https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv -P ./geo_data
+#wget https://s3.amazonaws.com/nyc-tlc/misc/taxi_zones.zip
+#unzip ./taxi_zones.zip
 
 #Yellow cab data
 wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2017-07.csv -P ./taxi_data
@@ -51,15 +51,6 @@ echo ${f}
 grep '^[0-9]' ${f} | awk -F',' -v OFS=',' '{print $1,$2,$3,$6,$7,$8,$9,$17}' >> ./taxi_data/consolidated_green_tripdata.csv
 done
 
-#Weather data
-#Open NYC weather data from https://www.ncdc.noaa.gov/
-#TODO - Make a curl command to download the data
-
-#Lat/long by address
-#Open address data from http://results.openaddresses.io
-#See also https://github.com/openaddresses/openaddresses/blob/adaa71fc0affa5c3f577b85825d341871cf3d3bd/sources/us/ny/city_of_new_york.json
-#and https://data.cityofnewyork.us/City-Government/NYC-Address-Points/g6pj-hd8k
-#TODO - Make a curl command to download the data (if needed??)
 
 
 
